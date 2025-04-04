4ce9d574138ffb6ec39cc1401b63c4eabea89aa0
evan <evanz1215@gmail.com>
2025-04-04 20:26:54 +0800

    更新身份驗證配置，將 IdentityServerUrl 更改為 IdentityServiceUrl，並修正 GatewayService 的設定檔

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddReverseProxy()
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        options.Authority = builder.Configuration["IdentityServerUrl"];
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
         options.RequireHttpsMetadata = false;
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";

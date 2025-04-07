2b8d113f4780a73dc379262c5dcd7f5aa338d3cb
evan <evanz1215@gmail.com>
2025-04-08 04:47:52 +0800

    feat: integrate Flowbite React and add pagination component
    - Updated package.json to include flowbite-react and added postinstall script.
    - Created .gitignore for Flowbite React configuration files.
    - Added Flowbite React configuration file with default settings.
    - Configured VSCode settings for Tailwind CSS support.
    - Implemented auctionActions to fetch auction data from the server.
    - Developed AppPagination component using Flowbite React for pagination functionality.

---
+++ frontend/web-app/app/components/AppPagination.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Pagination } from "flowbite-react";
+import React from "react";
+
+type Props = {
+  currentPage: number;
+  pageCount: number;
+  pageChange: (page: number) => void;
+};
+
+export default function AppPagination({
+  currentPage,
+  pageCount,
+  pageChange,
+}: Props) {
+  return (
+    <Pagination
+      currentPage={currentPage}
+      onPageChange={(e) => pageChange(e)}
+      totalPages={pageCount}
+      layout="pagination"
+      showIcons={true}
+      className="text-blue-500 mb-5"
+    />
+  );
+}
--- frontend/web-app/next.config.ts
+++ frontend/web-app/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,4 +20,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withFlowbiteReact(nextConfig);

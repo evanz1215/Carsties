24eb474b908d2c5e68feb6e3b5c4d62c3e604ba6
evan <evanz1215@gmail.com>
2025-04-10 02:02:50 +0800

    feat: 更新 Logo 組件以重置路由，並修正 Navbar 和 UserActions 組件以傳遞用戶資訊

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import { useParamsStore } from "@/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
+
 import { AiOutlineCar } from "react-icons/ai";
 
 export default function Logo() {
   const reset = useParamsStore((state) => state.reset);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function doRest() {
+    if (pathname !== "/") {
+      router.push("/");
+    }
+    reset();
+  }
 
   return (
     <div
-      onClick={reset}
+      onClick={doRest}
       className="cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500"
     >
       <AiOutlineCar size={34} />
--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -11,7 +11,7 @@ export default async function Navbar() {
     <header className="sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800">
       <Logo />
       <Search />
-      {user ? <UserActions /> : <LoginButton />}
+      {user ? <UserActions user={user} /> : <LoginButton />}
     </header>
   );
 }

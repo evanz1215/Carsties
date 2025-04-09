e5b6e20bc7523f399312d134f68e1f719af4a106
evan <evanz1215@gmail.com>
2025-04-09 15:11:46 +0800

    feat: 更新 Heading 組件以使 subtitle 成為可選，並新增 Navbar 和 UserActions 組件以整合用戶身份驗證

---
+++ frontend/web-app/app/session/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@/auth";
+import Heading from "../components/Heading";
+
+export default async function page() {
+  const session = await auth();
+  return (
+    <div>
+      <Heading title="Session dashboard" />
+      <div className="bg-blue-200 border-2 border-blue-500">
+        <h3 className="text-lg">Session data</h3>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
